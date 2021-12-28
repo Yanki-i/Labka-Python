acc8168ed1994ef0af9fee77b85168012e504b5e
Yanki-i <93607046+Yanki-i@users.noreply.github.com>
2021-12-28 16:20:21 +0200

    Add files via upload

---
+++ Labka-№8/Лабка№8.1.py
@@ -0,0 +1,11 @@
+def f(x):
+    if x < 23:
+        return 0
+    elif x == 23:
+        return 1
+    elif x > 23:
+        return -1
+a = float(input("Введіть значення а :"))
+b = float(input("Введіть значення b :"))
+suma = f(4)+f(a)+f(b)
+print(suma)
